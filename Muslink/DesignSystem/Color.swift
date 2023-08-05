d7a94b2e2b37a8d36769fbd4e59e5cc04a64b581
Aisha Nurgaliyeva <ayshanurgali@gmail.com>
2023-08-05 11:35:05 +0600

    modified colors

--- Muslink/DesignSystem/Color.swift
+++ Muslink/DesignSystem/Color.swift
@@ -65,100 +65,100 @@ extension Color {
 
         switch self {
         case .neutral100:
-            color = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
+            color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
         case .neutral80:
-            color = UIColor(red: 255, green: 255, blue: 255, alpha: 0.8)
+            color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
         case .neutral72:
-            color = UIColor(red: 255, green: 255, blue: 255, alpha: 0.72)
+            color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.72)
         case .neutral32:
-            color = UIColor(red: 255, green: 255, blue: 255, alpha: 0.32)
+            color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.32)
         case .neutral16:
-            color = UIColor(red: 255, green: 255, blue: 255, alpha: 0.16)
+            color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.16)
             
             
         case .primaryBgColor:
-            color = UIColor(red: 21, green: 24, blue: 36, alpha: 1)
+            color = UIColor(red: 21/255, green: 24/255, blue: 36/255, alpha: 1)
         case .elevatedBgColor:
-            color = UIColor(red: 34, green: 39, blue: 56, alpha: 1)
+            color = UIColor(red: 34/255, green: 39/255, blue: 56/255, alpha: 1)
         case .bgColor20:
-            color = UIColor(red: 100, green: 136, blue: 180, alpha: 0.2)
+            color = UIColor(red: 100/255, green: 136/255, blue: 180/255, alpha: 0.2)
         case .primaryMain:
-            color = UIColor(red: 184, green: 126, blue: 238, alpha: 1)
+            color = UIColor(red: 184/255, green: 126/255, blue: 238/255, alpha: 1)
         case .primaryPressed:
-            color = UIColor(red: 34, green: 15, blue: 128, alpha: 1)
+            color = UIColor(red: 34/255, green: 15/255, blue: 128/255, alpha: 1)
         case .primaryHover:
-            color = UIColor(red: 105, green: 77, blue: 249, alpha: 1)
+            color = UIColor(red: 105/255, green: 77/255, blue: 249/255, alpha: 1)
         case .primaryFocus:
-            color = UIColor(red: 226, green: 220, blue: 255, alpha: 1)
+            color = UIColor(red: 226/255, green: 220/255, blue: 255/255, alpha: 1)
         case .primaryBorder:
-            color = UIColor(red: 188, green: 176, blue: 245, alpha: 1)
+            color = UIColor(red: 188/255, green: 176/255, blue: 245/255, alpha: 1)
             
             
         case .accentMain:
-            color = UIColor(red: 254, green: 211, blue: 44, alpha: 1)
+            color = UIColor(red: 254/255, green: 211/255, blue: 44/255, alpha: 1)
         case .accentPressed:
-            color = UIColor(red: 222, green: 177, blue: 1, alpha: 1)
+            color = UIColor(red: 222/255, green: 177/255, blue: 1/255, alpha: 1)
         case .accentHover:
-            color = UIColor(red: 255, green: 219, blue: 80, alpha: 1)
+            color = UIColor(red: 255/255, green: 219/255, blue: 80/255, alpha: 1)
         case .accentFocus:
-            color = UIColor(red: 255, green: 240, blue: 180, alpha: 1)
+            color = UIColor(red: 255/255, green: 240/255, blue: 180/255, alpha: 1)
         case .accentBorder:
-            color = UIColor(red: 255, green: 229, blue: 129, alpha: 1)
+            color = UIColor(red: 255/255, green: 229/255, blue: 129/255, alpha: 1)
             
         
         case .successMain:
-            color = UIColor(red: 33, green: 114, blue: 94, alpha: 1)
+            color = UIColor(red: 33/255, green: 114/255, blue: 94/255, alpha: 1)
         case .successBgColor:
-            color = UIColor(red: 231, green: 250, blue: 245, alpha: 1)
+            color = UIColor(red: 231/255, green: 250/255, blue: 245/255, alpha: 1)
         case .successPressed:
-            color = UIColor(red: 22, green: 76, blue: 63, alpha: 1)
+            color = UIColor(red: 22/255, green: 76/255, blue: 63/255, alpha: 1)
         case .successHover:
-            color = UIColor(red: 27, green: 95, blue: 78, alpha: 1)
+            color = UIColor(red: 27/255, green: 95/255, blue: 78/255, alpha: 1)
         case .successFocus:
-            color = UIColor(red: 200, green: 235, blue: 226, alpha: 1)
+            color = UIColor(red: 200/255, green: 235/255, blue: 226/255, alpha: 1)
         case .successBorder:
-            color = UIColor(red: 181, green: 208, blue: 201, alpha: 1)
+            color = UIColor(red: 181/255, green: 208/255, blue: 201/255, alpha: 1)
         
             
         case .warningMain:
-            color = UIColor(red: 224, green: 206, blue: 44, alpha: 1)
+            color = UIColor(red: 224/255, green: 206/255, blue: 44/255, alpha: 1)
         case .warningBgColor:
-            color = UIColor(red: 254, green: 250, blue: 220, alpha: 1)
+            color = UIColor(red: 254/255, green: 250/255, blue: 220/255, alpha: 1)
         case .warningPressed:
-            color = UIColor(red: 149, green: 137, blue: 29, alpha: 1)
+            color = UIColor(red: 149/255, green: 137/255, blue: 29/255, alpha: 1)
         case .warningHover:
-            color = UIColor(red: 187, green: 172, blue: 37, alpha: 1)
+            color = UIColor(red: 187/255, green: 172/255, blue: 37/255, alpha: 1)
         case .warningFocus:
-            color = UIColor(red: 253, green: 248, blue: 207, alpha: 1)
+            color = UIColor(red: 253/255, green: 248/255, blue: 207/255, alpha: 1)
         case .warningBorder:
-            color = UIColor(red: 245, green: 239, blue: 185, alpha: 1)
+            color = UIColor(red: 245/255, green: 239/255, blue: 185/255, alpha: 1)
             
         case .dangerMain:
-            color = UIColor(red: 224, green: 81, blue: 81, alpha: 1)
+            color = UIColor(red: 224/255, green: 81/255, blue: 81/255, alpha: 1)
         case .dangerBgColor:
-            color = UIColor(red: 255, green: 232, blue: 232, alpha: 1)
+            color = UIColor(red: 255/255, green: 232/255, blue: 232/255, alpha: 1)
         case .dangerPressed:
-            color = UIColor(red: 84, green: 18, blue: 18, alpha: 1)
+            color = UIColor(red: 84/255, green: 18/255, blue: 18/255, alpha: 1)
         case .dangerHover:
-            color = UIColor(red: 140, green: 31, blue: 31, alpha: 1)
+            color = UIColor(red: 140/255, green: 31/255, blue: 31/255, alpha: 1)
         case .dangerFocus:
-            color = UIColor(red: 255, green: 223, blue: 223, alpha: 1)
+            color = UIColor(red: 255/255, green: 223/255, blue: 223/255, alpha: 1)
         case .dangerBorder:
-            color = UIColor(red: 226, green: 182, blue: 182, alpha: 1)
+            color = UIColor(red: 226/255, green: 182/255, blue: 182/255, alpha: 1)
             
             
         case .infoMain:
-            color = UIColor(red: 0, green: 35, blue: 221, alpha: 1)
+            color = UIColor(red: 0/255, green: 35/255, blue: 221/255, alpha: 1)
         case .infoBgColor:
-            color = UIColor(red: 235, green: 238, blue: 255, alpha: 1)
+            color = UIColor(red: 235/255, green: 238/255, blue: 255/255, alpha: 1)
         case .infoPressed:
-            color = UIColor(red: 0, green: 23, blue: 147, alpha: 1)
+            color = UIColor(red: 0/255, green: 23/255, blue: 147/255, alpha: 1)
         case .infoHover:
-            color = UIColor(red: 0, green: 29, blue: 184, alpha: 1)
+            color = UIColor(red: 0/255, green: 29/255, blue: 184/255, alpha: 1)
         case .infoFocus:
-            color = UIColor(red: 226, green: 231, blue: 255, alpha: 1)
+            color = UIColor(red: 226/255, green: 231/255, blue: 255/255, alpha: 1)
         case .infoBorder:
-            color = UIColor(red: 170, green: 182, blue: 244, alpha: 1)
+            color = UIColor(red: 170/255, green: 182/255, blue: 244/255a, alpha: 1)
         }
 
         return color ?? UIColor()
