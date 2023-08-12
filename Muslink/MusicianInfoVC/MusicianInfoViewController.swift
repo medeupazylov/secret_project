0ba9ecfcd856353c706a516364aedb7101be8ede
Aisha Nurgaliyeva <ayshanurgali@gmail.com>
2023-08-12 19:26:24 +0600

    CollectionView + PageControl

--- Muslink/MusicianInfoVC/MusicianInfoViewController.swift
+++ Muslink/MusicianInfoVC/MusicianInfoViewController.swift
@@ -8,19 +8,44 @@
 
 import UIKit
 
-class MusicianInfoViewController: UIViewController {
+class MusicianInfoViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout , UICollectionViewDelegate {
     
     private lazy var titleLabel = DefaultLabel(text: "Об исполнителе", textColor: Color.neutral100.color, fontSize: 15, fontWeight: .bold)
     
-    private lazy var musicianPhotoView = MusicianPhotoView()
+    private lazy var musicianNameStack = MusicianNameStack()
     
-    private lazy var mainStack = VerticalStackView(arrangedSubviews: [titleLabel, musicianPhotoView, socialNetworkStack, descriptionLabel], spacing: 24)
+    private lazy var mainStack = VerticalStackView(arrangedSubviews: [titleLabel, photoView, pageControl, socialNetworkStack, descriptionLabel], spacing: 24)
+    
+    private var photoView: PhotoView {
+        let photoView = PhotoView()
+        photoView.addSubview(collectionView)
+        photoView.addSubview(musicianNameStack)
+        photoView.heightAnchor.constraint(equalToConstant: 278).isActive = true
+        musicianNameStack.centerXAnchor.constraint(equalTo: collectionView.centerXAnchor).isActive = true
+        musicianNameStack.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: -24).isActive = true
+        return photoView
+    }
     
     private lazy var socialNetworkStack = SocialNetworksStackView()
+    
+    private var pageControl: UIPageControl {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = images.count
+        pageControl.pageIndicatorTintColor = Color.neutral16.color
+        pageControl.currentPageIndicatorTintColor = Color.neutral100.color
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.addTarget(self, action: #selector(pageControlValueChanged), for: .valueChanged)
+        pageControl.isUserInteractionEnabled = true
+        return pageControl
+    }
+    
+    
+    //MARK: - Functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupConstraints()
     }
     
     func setupNavigationBar() {
@@ -42,7 +67,6 @@ class MusicianInfoViewController: UIViewController {
         label.textColor = Color.neutral72.color
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        
         return label
     }
     
@@ -53,19 +77,85 @@ class MusicianInfoViewController: UIViewController {
         mainStack.distribution = .fillProportionally
         mainStack.alignment = .fill
         setupNavigationBar()
-        setupConstraints()
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
             mainStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
             mainStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            mainStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-//            mainStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30)
+            mainStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
         ])
-//        NSLayoutConstraint.activate([
-//            musicianPhotoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-//            musicianPhotoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
-//        ])
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            collectionView.heightAnchor.constraint(equalToConstant: 278)
+        ])
     }
+    
+    @objc func pageControlValueChanged() {
+        collectionView.scrollToItem(at: IndexPath(item: pageControl.currentPage, section: 0), at: .centeredHorizontally, animated: true)
+        print("VALUE CHANGED")
+        
+    }
+    
+    
+    //MARK: - CollectionView
+    
+    private var images: [String] = ["musician", "musician2", "musician3"]
+    private let cellIdentifier = "PhotoCarouselCell"
+        
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isPagingEnabled = true
+        collectionView.register(PhotoCarouselCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.layer.cornerRadius = 10
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+
+        //MARK: - UICollectionViewDataSource
+
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return images.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! PhotoCarouselCell
+            cell.imageView.image = UIImage(named: images[indexPath.item])
+            pageControl.currentPage = indexPath.row
+            return cell
+        }
+        
+        //MARK: - UICollectionViewDelegateFlowLayout
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return collectionView.bounds.size
+        }
+    
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let contentOffsetX = scrollView.contentOffset.x
+            let pageWidth = scrollView.bounds.width
+            let currentPage = Int(contentOffsetX / pageWidth)
+        
+            print("Content Offset X: \(contentOffsetX)")
+            print("Page Width: \(pageWidth)")
+            print("Current Page: \(currentPage)")
+        
+            pageControl.currentPage = currentPage
+    }
+    
+        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+            let width = scrollView.frame.width
+            pageControl.currentPage = Int(scrollView.contentOffset.x / width)
+            print("AAA")
+        }
+    
+    
+    
 }
